c9a974fffe58cb93db4e9d10f094505bcea5e25e
suntong <1730353402@qq.com>
2020-04-02 18:39:51 +0800

    banner

--- main.js
+++ main.js
@@ -1,27 +1,4 @@
-requirejs.config({
-  paths: {
-    jquery: "https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min"
-  }
-});
-require(["jquery"], function($) {
-  $(function() {
-    require(['carousel'],function(Carousel){
-      var car=new Carousel()
-      car.animate({
-        container:'#box',
-        time:1000,
-        img:[
-          'img/b1.png',
-          'img/b2.png',
-          'img/b3.png',
-          'img/b4.png',
-          'img/b5.png'
-        ]
-      });
-    })
-  });
-});
-/*
+
 equirejs.config({
   paths: {
     jquery: "https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min"
@@ -44,4 +21,4 @@ require(["jquery"], function($) {
     })
   });
 });
-*/
+
