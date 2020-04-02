73d6dc5513f813d97534e04febfd8dcb9b96974f
suntong <1730353402@qq.com>
2020-04-02 18:20:05 +0800

    ceshi

--- main.js
+++ main.js
@@ -5,6 +5,30 @@ requirejs.config({
 });
 require(["jquery"], function($) {
   $(function() {
+    require(['carousel'],function(Carousel){
+      var car=new Carousel()
+      car.animate({
+        container:'#box',
+        time:1000,
+        img:[
+          'img/b1.png',
+          'img/b2.png',
+          'img/b3.png',
+          'img/b4.png',
+          'img/b5.png'
+        ]
+      });
+    })
+  });
+});
+/*
+equirejs.config({
+  paths: {
+    jquery: "https://apps.bdimg.com/libs/jquery/2.1.4/jquery.min"
+  }
+});
+require(["jquery"], function($) {
+  $(function() {
     require(['carousel'],function(carousel){
       new carousel({
         container:'#box',
@@ -20,3 +44,4 @@ require(["jquery"], function($) {
     })
   });
 });
+*/
